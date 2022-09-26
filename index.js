c9a5aaa6d69522e03e0bd212e14d522169b6c930
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-09-26 00:03:01 -0500

    Add cors.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const { connect } = require('./db/mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 ;(async () => {
   const app = express()
   const port = 4001
 
   app.use(express.json())
-
+  app.use(cors())
   await connect()
 
   routes(app)
