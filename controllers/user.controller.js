33542a0ca7a3e312d7d3077bb2570cedbeaf4767
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-10-08 21:15:44 -0500

    Update Model User.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,7 +14,9 @@ const getUsers = async (req = request, res = response) => {
 
   const [total, users] = await Promise.all([
     User.countDocuments(query),
-    User.find(query).skip(Number(from)).limit(Number(limit)),
+    User.find(query),
+    //* GET USERS WITH PAGINATED
+    // User.find(query).skip(Number(from)).limit(Number(limit)),
   ])
   res.json({
     users,
@@ -30,14 +32,26 @@ const getUser = async (req, res) => {
 
 const postUsers = async (req, res = response) => {
   const { name, email, password, role } = req.body
-
+  let rating
+  if (role === 'MENTOR_ROLE') {
+    rating = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      average: 0,
+    }
+  }
   const user = new User({
     name,
     email,
     password,
     role,
+    rating,
   })
 
+  console.log({ user })
   const salt = bcryptjs.genSaltSync()
   user.password = bcryptjs.hashSync(password, salt)
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,44 @@
 const { Schema, model } = require('mongoose')
 
-const UserSchema = Schema({
+const CommentSchema = new Schema({
+  content: {
+    type: String,
+    default: '',
+  },
+  author: {
+    type: String,
+    default: '',
+  },
+})
+
+const RatingSchema = new Schema({
+  1: {
+    type: Number,
+    default: 0,
+  },
+  2: {
+    type: Number,
+    default: 0,
+  },
+  3: {
+    type: Number,
+    default: 0,
+  },
+  4: {
+    type: Number,
+    default: 0,
+  },
+  5: {
+    type: Number,
+    default: 0,
+  },
+  average: {
+    type: Number,
+    default: 0,
+  },
+})
+
+const UserSchema = new Schema({
   name: {
     type: String,
     required: [true, 'Name is required'],
@@ -13,24 +51,50 @@ const UserSchema = Schema({
     type: String,
     required: [true, 'Password is required'],
   },
-  photoURL: {
-    type: String,
-    default: '',
-  },
-  profession: {
-    type: String,
-    default: '',
-  },
-  bio: {
-    type: String,
-    default: '',
-  },
   role: {
     type: String,
     required: true,
     default: 'STUDENT_ROLE',
     enum: ['MENTOR_ROLE', 'STUDENT_ROLE'],
   },
+  about: {
+    type: String,
+    default: '',
+  },
+  career: {
+    type: String,
+    default: '',
+  },
+  university: {
+    type: String,
+    default: '',
+  },
+  job: {
+    type: String,
+    default: '',
+  },
+  experience: {
+    type: String,
+    default: '',
+  },
+  nationality: {
+    type: String,
+    default: '',
+  },
+  genre: {
+    type: String,
+    default: '',
+  },
+  linkedin: {
+    type: String,
+    default: '',
+  },
+  photoURL: {
+    type: String,
+    default: '',
+  },
+  rating: RatingSchema,
+  comments: [CommentSchema],
 })
 
 UserSchema.methods.toJSON = function () {
