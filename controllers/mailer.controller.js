85a51dce2a7f600af4008e8d83558e2478eda8bb
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-12-13 15:09:01 -0500

    Use handlebars to send email.

--- controllers/mailer.controller.js
+++ controllers/mailer.controller.js
@@ -1,46 +1,66 @@
 const { request, response } = require('express')
-const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars')
+const nodemailer = require('nodemailer')
 require('dotenv').config()
 
+const options = {
+  viewEngine: {
+    extname: '.handlebars', // handlebars extension
+    layoutsDir: 'views/email/', // location of handlebars templates
+    defaultLayout: 'template', // name of main template
+    partialsDir: 'views/email/', // location of your subtemplates aka. header, footer etc
+  },
+  viewPath: 'views/email',
+  extName: '.handlebars',
+}
+
 const postMailer = async (req = request, res = response) => {
-    const { to, subject, message } = req.body
+  const { to, subject, name, duration, date, hour, meet } = req.body
 
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
-      secure: process.env.MAIL_SECURE,
-      auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD
-          
-      }
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  })
 
-    let info = await transporter.sendMail({
-        from: process.env.MAIL_FROM_NAME + process.env.MAIL_FROM_ADDRESS, // sender address,
-        to: to,
-        subject: subject,
-        html: message
-    })
+  transporter.use('compile', hbs(options))
 
-    console.log('Message sent: %s', info.messageId);
-    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  
-    transporter.close(); 
+  let info = await transporter.sendMail({
+    from: process.env.MAIL_FROM_NAME + process.env.MAIL_FROM_ADDRESS, // sender address,
+    to: to,
+    subject: subject,
+    template: 'template',
+    context: {
+      name,
+      duration,
+      date,
+      hour,
+      meet,
+    },
+  })
 
-    res.json({
-      messageId: info.messageId,
-      previewUrl:  nodemailer.getTestMessageUrl(info)
-    })
-  }
+  console.log('Message sent: %s', info.messageId)
+  // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-  const GetMailer = async (req = request, res = response) => {
-    res.json({
-      message: 'Hello World!',
-    })
-  }
+  transporter.close()
 
-  module.exports = {
-    postMailer,
-    GetMailer,
-  }
+  res.json({
+    messageId: info.messageId,
+    previewUrl: nodemailer.getTestMessageUrl(info),
+  })
+}
+
+const GetMailer = async (req = request, res = response) => {
+  res.json({
+    message: 'Hello World!',
+  })
+}
+
+module.exports = {
+  postMailer,
+  GetMailer,
+}
