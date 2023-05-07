83aa6015ea2921ecae32feea44275f0f57ead479
Jack Sari <janasarii@gmail.com>
2023-05-07 02:45:05 -0500

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    apps : [
-        {
-            name: "app",
-            script: 'npm',
-            args: 'run start',
-        },
-
-    ]
+  apps: [
+    {
+      name: 'app',
+      script: 'npm',
+      args: 'run start',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 }
